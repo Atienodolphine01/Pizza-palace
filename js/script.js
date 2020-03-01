347ac1874eb07d5aaccd4e9b57ffcb3ba3df9741
Dolphine Atieno Awuor <awuordolphine65@gmail.com>
2020-03-01 04:36:34 +0300

    edit h1 in html

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$("#sign").click(function() {
+		$("#rasp").toggle();
+		$("#").toggle();
+	});
+	$("#dev").click(function(){
+		$("#development").toggle();
+		$("#para2").toggle();
+	});
+	$("#manage").click(function(){
+		$("#product").toggle();
+		$("#para3").toggle();
+	});
+});
