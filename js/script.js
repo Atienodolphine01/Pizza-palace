8c704f31e6521090ce3c342325cc55bdceca1c30
Dolphine Atieno Awuor <awuordolphine65@gmail.com>
2020-03-01 10:47:09 +0300

    remote id make in button

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
-$(document).ready(function(){
-	$("#sign").click(function() {
-		$("#rasp").toggle();
-		$("#").toggle();
-	});
-	$("#dev").click(function(){
-		$("#development").toggle();
-		$("#para2").toggle();
-	});
-	$("#manage").click(function(){
-		$("#product").toggle();
-		$("#para3").toggle();
-	});
-});
+// $(document).ready(function(){
+// 	$("#sign").click(function() {
+// 		$("#rasp").toggle();
+// 		$("#").toggle();
+// 	});
+// 	$("#dev").click(function(){
+// 		$("#development").toggle();
+// 		$("#para2").toggle();
+// 	});
+// 	$("#manage").click(function(){
+// 		$("#product").toggle();
+// 		$("#para3").toggle();
+// 	});
+// });
+$(function(){
+    $('.sum').hide();
+    $('.data').hide();
+    
+})
